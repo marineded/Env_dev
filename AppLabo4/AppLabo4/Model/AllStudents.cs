7f3d63e161530b37e30f21efe39a5dd4b94f1ac3
marineded <marineded@gmail.com>
2016-10-03 12:19:21 +0200

    LAbo4

---
+++ AppLabo4/AppLabo4/Model/AllStudents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLabo4.Model
+{
+    public static class AllStudents
+    {
+        public static IEnumerable<Student> Students { get; set;}
+
+        public static IEnumerable<Student> GetAllStudents()
+        {
+            return Students = new List<Student>
+            {
+                new Student("Student 1 ",20),
+                new Student("Student 2", 19),
+                new Student("Student 3", 21)
+            };
+        }
+    }
+}
---
+++ AppLabo4/AppLabo4/Model/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLabo4.Model
+{
+    public class Student
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(String name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
