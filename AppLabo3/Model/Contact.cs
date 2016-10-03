98781bea7f4a8dea9442553d251b64b91594b834
marineded <marineded@gmail.com>
2016-10-03 11:27:11 +0200

    Labo3_Env

---
+++ AppLabo3/Model/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Contact
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+
+        public Contact(String firstName, String lastName, String email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
